bd15ec827c3e612644c9a200830d57d2bbf04eff
doraco <foyfoymaru@gmail.com>
2022-01-10 18:44:11 +0900

    ステージ選択処理 ・ボタンを矢印アイコンに変更 ・ステージテキスト、画像切り替え ・選択できない場合の前/次ボタンの非表示化（透明化） ・クリアステージ数読込 ・ステージ選択時の遷移追加（遷移先シーンがないので仮でタイトルに戻す）
    設定ボタン
    ・イメージを背景＋歯車に変更
    
    タイトル
    ・フォント変更
    ・ファイルリネーム:EventSystem.cs → TitleManager.cs
    ・画面遷移まで0.5秒の待ち時間を追加

---
+++ Assets/Scripts/StageSelect.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StageSelect : MonoBehaviour {
+    private const int MIN_STAGE = 1;    // ステージ数最小
+    private const int MAX_STAGE = 10;   // ステージ数最大
+
+    public int stageNumber = 1;         // 選択中ステージNo.
+    private int stageClearNumber;       // クリア済みステージ数
+
+    public GameObject stageNumberText;  // ステージNo.テキスト
+    public GameObject stagePrevButton;  // ステージ前へボタン
+    public GameObject stageNextButton;  // ステージ次へボタン
+    public GameObject stageEnterButton; // ステージ決定ボタン
+
+    public Sprite[] stagePicture = new Sprite[MAX_STAGE]; //ステージ画像
+
+
+    // Start is called before the first frame update
+    void Start() {
+        // クリアステージNo.をロード
+        stageClearNumber = PlayerPrefs.GetInt("CLEAR_STAGE_NO", 0);
+        stageNumber = stageClearNumber+1;
+        StageDispRefresh();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    // 前ステージ選択
+    public void StageSelectPrev() {
+        if (MIN_STAGE >= stageNumber) return;
+        stageNumber--;
+        StageDispRefresh();
+    }
+
+    // 次ステージ選択
+    public void StageSelectNext() {
+        if (stageClearNumber+1 <= stageNumber) return;
+        stageNumber++;
+        StageDispRefresh();
+    }
+
+    // ステージ決定
+    public void StageSelectEnter() {
+        StartCoroutine(StageEnterCoroutine());
+    }
+
+    // 選択ステージ描画更新
+    private void StageDispRefresh() {
+        // 前・次ボタンが不要なときボタンを非表示にする
+        // LayoutGroupが崩れるので無効化はしない
+        // todo:もっと上手くアルファ値だけ変える方法があればそれに変更する
+        var color = stagePrevButton.GetComponent<Image>().color;
+        stagePrevButton.GetComponent<Image>().color
+            = MIN_STAGE < stageNumber
+                ? new Color(color.r, color.g, color.b, 1.0f)
+                : new Color(color.r, color.g, color.b, 0.0f);
+        stageNextButton.GetComponent<Image>().color
+            = stageClearNumber + 1 > stageNumber
+                ? new Color(color.r, color.g, color.b, 1.0f)
+                : new Color(color.r, color.g, color.b, 0.0f);
+
+        // ステージNo.テキストを更新
+        stageNumberText.GetComponent<Text>().text = "Stage " + stageNumber.ToString("D2");
+
+        // ステージ画像を更新
+        stageEnterButton.GetComponent<Image>().sprite = stagePicture[stageNumber-1];
+
+    }
+
+    // ステージ決定ボタン押下後0.5秒後に遷移する
+    private IEnumerator StageEnterCoroutine() {
+        if (PlayerPrefs.GetInt("CLEAR_STAGE_NO", 0) < stageNumber) {
+            PlayerPrefs.SetInt("CLEAR_STAGE_NO", stageNumber);
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        //todo:ゲームステージへ遷移するように変更する
+        SceneManager.LoadScene("Title");
+
+        yield break;
+    }
+}
